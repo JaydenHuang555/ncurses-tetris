d3a784efc8fd38ff0209988d28943832c34a336e
JaydenHuang555 <JaydenkanaHuang@outlook.com>
2025-08-04 21:08:24 -0700

    added good graphic names

--- graphics/graphics.h
+++ graphics/graphics.h
@@ -6,12 +6,12 @@
 #include "bitdefs.h"
 
 struct graphics_t {
-   struct rectangle_t **g_rec;
-   size_t g_rec_size, g_rec_cap;
-   pthread_t g_redrawing_thread;
-   pthread_mutex_t g_thread_lock, *g_ncurses_mutex;
-   volatile s8 g_redrawing_thread_running;
-   WINDOW *g_drawing_window;
+   struct rectangle_t **rec;
+   size_t rec_size, rec_cap;
+   pthread_t redrawing_thread;
+   pthread_mutex_t thread_lock, *ncurses_mutex;
+   volatile s8 redrawing_thread_running;
+   WINDOW *drawing_window;
 };
 
 extern void graphics_start(struct graphics_t *, pthread_mutex_t*);
